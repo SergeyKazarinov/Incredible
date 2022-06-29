0b9e0bdd5048fd3d2bb18f89fb7c280821a74ec7
Sergey Kazarinov <kazarinov092@gmail.com>
2022-06-29 15:35:01 +0500

    feat: add index.js

---
+++ utils/utils.js
@@ -0,0 +1,8 @@
+export const validConfiguration = {
+  formSelector: 'form',
+  inputSelector: 'form__input',
+  submitButtonSelector: 'button_type_submit',
+  inactiveButtonClass: 'button_inactive',
+  inputErrorClass: 'form__input_type_error',
+  errorMessage: 'input-message-error_active',
+};
