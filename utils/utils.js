08fcfeeb5641232b79d71837565fb80c6b731e84
Sergey Kazarinov <kazarinov092@gmail.com>
2022-06-30 14:45:35 +0500

    feat(formvalidators): add validation of the form

--- utils/utils.js
+++ utils/utils.js
@@ -4,5 +4,8 @@ export const validConfiguration = {
   submitButtonSelector: 'button_type_submit',
   inactiveButtonClass: 'button_inactive',
   inputErrorClass: 'form__input_type_error',
-  errorMessage: 'input-message-error_active',
+  errorMessage: 'form__input-error_active',
 };
+
+export const formContact = document.querySelector('.contact__form');
+export const submitButton = document.querySelector('.button_type_submit');
